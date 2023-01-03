26db13728170fc64e6c23a5fe3a1ab00d94971c7
EunjoungAhn <aej9300@gmail.com>
2023-01-03 23:37:51 +0900

    buttons section

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 80,),
               Row(
@@ -38,6 +39,38 @@ class App extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+              SizedBox(height: 120,),
+              Text(
+                "Total Balance", 
+                style:  TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                  ),
+              ),
+              SizedBox(height: 10,),
+              Text(
+                "\$5 194 482", 
+                style:  TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600
+                  ),
+              ),
+              SizedBox(height: 30,),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      child: Text("Transfer", style: TextStyle(fontSize: 20),),
+                    ),
+                  ),
                 ],
               ),
             ]
