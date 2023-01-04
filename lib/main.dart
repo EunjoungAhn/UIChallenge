aeedb0a9e89e20aecd541747d13dda4fa9d6490a
EunjoungAhn <aej9300@gmail.com>
2023-01-04 23:28:13 +0900

    Widget life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,8 +35,12 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text("nothing"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
@@ -37,13 +49,36 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  // 오직 단 한번만 호출된다.
+  @override
+  void initState() {
+    // 최신 api를 업데이트 할때 등등
+    super.initState();
+    print("initState");
+  }
+  // build 빌드 메서드 보다 먼저 호출되어야 한다.
+
+  //dispose는 위젯이 스크린에서 제거될 때 호출되는 메서드다.
+  @override
+  void dispose() {
+    // api 업데이트나 이벤트 리스너로 부터 구독을 취소할때 등등
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build");
     return Text(
       "My Large Title",
       style: TextStyle(
